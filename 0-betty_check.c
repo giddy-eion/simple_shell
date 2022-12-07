a0dd28125e74b4abfb0e835d758658977025661a
giddy-eion <gideonosamudiame@gmail.com>
2022-12-07 15:30:56 +0000

    add readme file and 0-betty_check.c

---
+++ 0-betty_check.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a function that passes the betty code tester
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("test passed\n");
+
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+contains various programs in c language and function to run a simple shell in a unix like system
